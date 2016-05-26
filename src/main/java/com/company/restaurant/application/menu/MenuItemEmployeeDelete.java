46d07e8d1d50e7354c3579eab18db3aa4f0f046e
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-26 03:14:59 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/application/menu/MenuItemEmployeeDelete.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemEmployeeDelete.java
@@ -43,7 +43,6 @@ public class MenuItemEmployeeDelete extends MenuItemEmployeeFind implements Menu
                 if (someDataWasSuccessfullyDeleted) {
                     dataHasBeenSuccessfullyDeletedMessage();
                 }
-
             }
         }
     }
