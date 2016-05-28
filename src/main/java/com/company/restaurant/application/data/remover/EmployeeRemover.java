faa4f10e58bae0afd2122892289db7f76ec9a024
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-28 04:00:37 +0300

    JEE Unit 6.2 Homework

---
+++ src/main/java/com/company/restaurant/application/data/remover/EmployeeRemover.java
@@ -0,0 +1,19 @@
+package com.company.restaurant.application.data.remover;
+
+import com.company.restaurant.application.data.service.ObjectChooser;
+import com.company.restaurant.application.data.service.ObjectRemoverProto;
+import com.company.restaurant.model.Employee;
+
+/**
+ * Created by Yevhen on 28.05.2016.
+ */
+public class EmployeeRemover extends ObjectRemoverProto<Employee> {
+    public EmployeeRemover(ObjectChooser<Employee> objectChooser) {
+        super(objectChooser);
+    }
+
+    @Override
+    protected String delObject(Employee employee) {
+        return getRestaurantController().delEmployee(employee);
+    }
+}
--- src/main/java/com/company/restaurant/application/menu/MenuItemEmployeeDelete.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemEmployeeDelete.java
@@ -1,50 +1,21 @@
 package com.company.restaurant.application.menu;
 
+import com.company.restaurant.application.data.chooser.EmployeeChooser;
+import com.company.restaurant.application.data.list.EmployeeTableList;
+import com.company.restaurant.application.data.remover.EmployeeRemover;
+import com.company.restaurant.application.menu.service.DatabaseMenuItem;
 import com.company.restaurant.application.menu.service.MenuItem;
-import com.company.restaurant.model.Employee;
-import com.company.util.Util;
-
-import java.util.List;
 
 /**
  * Created by Yevhen on 25.05.2016.
  */
-public class MenuItemEmployeeDelete extends MenuItemEmployeeFind implements MenuItem {
+public class MenuItemEmployeeDelete extends DatabaseMenuItem implements MenuItem {
     public MenuItemEmployeeDelete(String itemText) {
         super(itemText);
     }
 
     @Override
-    protected void executeAction() {
-        deleteEmployee();
-    }
-
-    private void deleteEmployee() {
-        // Show all data
-        tableList();
-
-        String[] employeeName = readEmployeeName();
-        List<Employee> data = findData();
-
-        if (data == null || data.size() == 0) {
-            oneObjectHasNotBeenFoundMessage();
-        } else {
-            if (!employeeName[0].isEmpty() || !employeeName[1].isEmpty()) {
-                boolean wasErrorDetected;
-                boolean someDataWasSuccessfullyDeleted = false;
-                for (Employee employee : data) {
-                    String errorMessage = getRestaurantController().delEmployee(employee);
-                    wasErrorDetected = errorMessage != null && !errorMessage.isEmpty();
-                    if (wasErrorDetected) {
-                        Util.printMessage(errorMessage);
-                    } else {
-                        someDataWasSuccessfullyDeleted = true;
-                    }
-                }
-                if (someDataWasSuccessfullyDeleted) {
-                    dataHasBeenSuccessfullyDeletedMessage();
-                }
-            }
-        }
+    protected void performAction() {
+        new EmployeeRemover(new EmployeeChooser(new EmployeeTableList())).deleteObject();
     }
 }
