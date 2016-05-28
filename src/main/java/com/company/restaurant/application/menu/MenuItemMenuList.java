f5d112e736a70e14c1bd326867a66c9ca163282a
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-28 04:47:44 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/application/menu/MenuItemMenuList.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemMenuList.java
@@ -1,35 +1,19 @@
 package com.company.restaurant.application.menu;
 
+import com.company.restaurant.application.data.list.MenuTableList;
+import com.company.restaurant.application.menu.service.DatabaseMenuItem;
 import com.company.restaurant.application.menu.service.MenuItem;
-import com.company.restaurant.model.Menu;
-
-import java.util.ArrayList;
 
 /**
  * Created by Yevhen on 25.05.2016.
  */
-public abstract class MenuItemMenuList extends ExtendedDatabaseMenuItem<Menu> implements MenuItem {
-    private static final String[] menuListHeader = new String[] {
-            "Menu Id",
-            "Menu name"
-    };
-
+public class MenuItemMenuList extends DatabaseMenuItem implements MenuItem {
     public MenuItemMenuList(String itemText) {
         super(itemText);
     }
 
     @Override
-    protected String[] getListHeader() {
-        return menuListHeader;
-    }
-
-    @Override
-    protected String[] dataSetRowDataToStringArray(Menu menu) {
-        ArrayList<String> arrayList = new ArrayList<>();
-
-        arrayList.add(Integer.toString(menu.getId()));
-        arrayList.add(menu.getName());
-
-        return arrayList.toArray(new String[arrayList.size()]);
+    protected void performAction() {
+        new MenuTableList().displayObjectList();
     }
 }
