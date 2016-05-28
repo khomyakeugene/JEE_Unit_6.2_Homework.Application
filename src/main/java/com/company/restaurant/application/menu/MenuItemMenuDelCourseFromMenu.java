e858fdaa1d706d50a5ac6dd014ca0fbe0763fbf9
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-28 13:18:07 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/application/menu/MenuItemMenuDelCourseFromMenu.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemMenuDelCourseFromMenu.java
@@ -1,5 +1,10 @@
 package com.company.restaurant.application.menu;
 
+import com.company.restaurant.application.data.MenuCourseCollector;
+import com.company.restaurant.application.data.chooser.CourseChooser;
+import com.company.restaurant.application.data.chooser.MenuChooser;
+import com.company.restaurant.application.data.list.CourseTableList;
+import com.company.restaurant.application.data.list.MenuTableList;
 import com.company.restaurant.application.menu.service.DatabaseMenuItem;
 import com.company.restaurant.application.menu.service.MenuItem;
 
@@ -7,6 +12,9 @@ import com.company.restaurant.application.menu.service.MenuItem;
  * Created by Yevhen on 25.05.2016.
  */
 public class MenuItemMenuDelCourseFromMenu extends DatabaseMenuItem implements MenuItem {
+    private MenuCourseCollector menuCourseCollector = new MenuCourseCollector(new MenuChooser(new MenuTableList()),
+            new CourseChooser(new CourseTableList()));
+
     public MenuItemMenuDelCourseFromMenu(String itemText) {
         super(itemText);
     }
--- src/main/java/com/company/restaurant/application/menu/MenuItemOrderAddCourseToOrder.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemOrderAddCourseToOrder.java
@@ -1,18 +1,18 @@
 package com.company.restaurant.application.menu;
 
+import com.company.restaurant.application.menu.service.DatabaseMenuItem;
 import com.company.restaurant.application.menu.service.MenuItem;
-import com.company.restaurant.model.Order;
 
 /**
  * Created by Yevhen on 26.05.2016.
  */
-public class MenuItemOrderAddCourseToOrder extends MenuItemOrderOpenOrderOperation implements MenuItem {
+public class MenuItemOrderAddCourseToOrder extends DatabaseMenuItem implements MenuItem {
     public MenuItemOrderAddCourseToOrder(String itemText) {
         super(itemText);
     }
 
     @Override
-    protected String doActionOnDatabaseObject(Order object) {
-        return super.doActionOnDatabaseObject(object);
+    protected void performAction() {
+
     }
 }
