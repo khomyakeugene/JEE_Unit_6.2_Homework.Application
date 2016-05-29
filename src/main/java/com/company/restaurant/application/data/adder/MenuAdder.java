90461d66e984b7af3a2d7ca354edf2af78c43c58
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-29 17:01:45 +0300

    JEE Unit 6.2 Homework

---
+++ src/main/java/com/company/restaurant/application/data/adder/MenuAdder.java
@@ -0,0 +1,46 @@
+package com.company.restaurant.application.data.adder;
+
+import com.company.restaurant.application.data.collector.ItemCollector;
+import com.company.restaurant.application.data.collector.MenuCourseCollector;
+import com.company.restaurant.application.data.list.MenuTableList;
+import com.company.restaurant.application.data.list.ObjectTableList;
+import com.company.restaurant.model.Menu;
+import com.company.util.Util;
+
+/**
+ * Created by Yevhen on 29.05.2016.
+ */
+public class MenuAdder extends ObjectAdderProto<Menu> implements ObjectAdder<Menu> {
+    private static final String ENTER_NAME_MESSAGE = "Please, enter menu name";
+
+    private ItemCollector<Menu> menuCourseCollector = MenuCourseCollector.newInstance();
+
+
+    public MenuAdder(ObjectTableList<Menu> objectTableList) {
+        super(objectTableList);
+    }
+
+    private Menu addMenu() {
+        Menu result = null;
+
+        String menuName = Util.readInputString(ENTER_NAME_MESSAGE);
+        if (menuName != null && !menuName.isEmpty()) {
+            result = getRestaurantController().addMenu(menuName.trim());
+            dataHasBeenSuccessfullyAddedMessage();
+        }
+
+        return result;
+    }
+
+    @Override
+    protected Menu addObject() {
+        Menu result = addMenu();
+        menuCourseCollector.addItemsToObject(result);
+
+        return result;
+    }
+
+    public static ObjectAdder<Menu> newInstance() {
+        return new MenuAdder(new MenuTableList());
+    }
+}
--- src/main/java/com/company/restaurant/application/menu/MenuItemMenuAdd.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemMenuAdd.java
@@ -1,38 +1,23 @@
 package com.company.restaurant.application.menu;
 
-import com.company.restaurant.application.data.collector.ItemCollector;
-import com.company.restaurant.application.data.collector.MenuCourseCollector;
+import com.company.restaurant.application.data.adder.MenuAdder;
+import com.company.restaurant.application.data.adder.ObjectAdder;
 import com.company.restaurant.application.menu.service.DatabaseMenuItem;
 import com.company.restaurant.application.menu.service.MenuItem;
 import com.company.restaurant.model.Menu;
-import com.company.util.Util;
 
 /**
  * Created by Yevhen on 25.05.2016.
  */
 public class MenuItemMenuAdd extends DatabaseMenuItem implements MenuItem {
-    private static final String ENTER_NAME_MESSAGE = "Please, enter menu name";
-
-    private ItemCollector<Menu> menuCourseCollector = MenuCourseCollector.newInstance();
+    private ObjectAdder<Menu> menuAdder = MenuAdder.newInstance();
 
     public MenuItemMenuAdd(String itemText) {
         super(itemText);
     }
 
-    private Menu addMenu() {
-        Menu result = null;
-
-        String menuName = Util.readInputString(ENTER_NAME_MESSAGE);
-        if (menuName != null && !menuName.isEmpty()) {
-            result = getRestaurantController().addMenu(menuName.trim());
-            dataHasBeenSuccessfullyAddedMessage();
-        }
-
-        return result;
-    }
-
     @Override
     protected void performAction() {
-        menuCourseCollector.addItemsToObject(addMenu());
+        menuAdder.addObjects();
     }
 }
