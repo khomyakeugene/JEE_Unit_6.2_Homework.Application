e45b1ec4ca0c58fe832778eb0e8336a265636ce8
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-01 00:09:01 +0300

    JEE Unit 6.2 Homework

---
+++ src/main/java/com/company/restaurant/application/data/converter/OrderCloser.java
@@ -0,0 +1,25 @@
+package com.company.restaurant.application.data.converter;
+
+import com.company.restaurant.application.data.chooser.ObjectChooser;
+import com.company.restaurant.application.data.chooser.OrderChooser;
+import com.company.restaurant.application.data.service.DatabaseService;
+import com.company.restaurant.model.Order;
+import com.company.util.Util;
+
+/**
+ * Created by Yevhen on 01.06.2016.
+ */
+public class OrderCloser extends DatabaseService {
+    private static final String ORDER_HAS_BEEN_CLOSED_PATTERN = "Order with number <%s> has been closed";
+
+    private ObjectChooser<Order> orderChooser = OrderChooser.newInstance();
+
+    public void closeOrder() {
+        Order order = orderChooser.chooseObjectFromList();
+        if (order != null) {
+            getRestaurantController().closeOrder(order);
+            Util.printMessage(String.format(ORDER_HAS_BEEN_CLOSED_PATTERN, order.getOrderNumber()));
+        }
+    }
+
+}
--- src/main/java/com/company/restaurant/application/menu/MenuItemOrderCloseOrder.java
+++ src/main/java/com/company/restaurant/application/menu/MenuItemOrderCloseOrder.java
@@ -1,28 +1,17 @@
 package com.company.restaurant.application.menu;
 
-import com.company.restaurant.application.data.chooser.ObjectChooser;
-import com.company.restaurant.application.data.chooser.OrderChooser;
-import com.company.restaurant.application.data.service.DatabaseService;
+import com.company.restaurant.application.data.converter.OrderCloser;
 import com.company.restaurant.application.menu.service.MenuItem;
 import com.company.restaurant.application.menu.service.SafeMenuItem;
-import com.company.restaurant.model.Order;
-import com.company.util.Util;
 
 /**
  * Created by Yevhen on 26.05.2016.
  */
 public class MenuItemOrderCloseOrder extends SafeMenuItem implements MenuItem {
-    private static final String ORDER_HAS_BEEN_CLOSED_PATTERN = "Order with number <%s> has been closed";
-
-    private DatabaseService databaseService = new DatabaseService();
-    private ObjectChooser<Order> orderChooser = OrderChooser.newInstance();
+    private OrderCloser orderCloser = new OrderCloser();
 
     @Override
     protected void performAction() {
-        Order order = orderChooser.chooseObjectFromList();
-        if (order != null) {
-            databaseService.getRestaurantController().closeOrder(order);
-            Util.printMessage(String.format(ORDER_HAS_BEEN_CLOSED_PATTERN, order.getOrderNumber()));
-        }
+        orderCloser.closeOrder();
     }
 }
