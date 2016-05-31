5df1b936d90cb094dd3317543e588953acc28d7e
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-01 00:12:59 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/application/RestaurantConsoleApplication.java
+++ src/main/java/com/company/restaurant/application/RestaurantConsoleApplication.java
@@ -10,30 +10,6 @@ import com.company.util.Util;
 public class RestaurantConsoleApplication {
     private static final String TITLE_MESSAGE = "Welcome to the restaurant application!";
     private static final String FAREWELL_MESSAGE = "Thank you for use our application";
-/*
-
-    private static final int WAREHOUSE_MENU_ITEM_CODE_ADD = 1;
-    private static final int WAREHOUSE_MENU_ITEM_CODE_DELETE = 2;
-    private static final int WAREHOUSE_MENU_ITEM_CODE_FIND = 3;
-    private static final int WAREHOUSE_MENU_ITEM_CODE_LIST = 4;
-    private static final int WAREHOUSE_MENU_ITEM_CODE_ELAPSING_LIST = 5;
-    private static final int WAREHOUSE_MENU_ITEM_CODE_EXIT = 6;
-    private static final String WAREHOUSE_MENU_ITEM_TEXT_ADD = "Add ingredient to warehouse";
-    private static final String WAREHOUSE_MENU_ITEM_TEXT_DELETE = "Take ingredient from warehouse";
-    private static final String WAREHOUSE_MENU_ITEM_TEXT_FIND = "Find ingredient in warehouse by name";
-    private static final String WAREHOUSE_MENU_ITEM_TEXT_LIST = "Get warehouse ingredient list";
-    private static final String WAREHOUSE_MENU_ITEM_TEXT_ELAPSING_LIST = "Get elapsing warehouse ingredient list";
-    private static final HashMap<Integer, MenuItem> warehouseMenuMap = new HashMap<Integer, MenuItem>(){
-        {
-            put(WAREHOUSE_MENU_ITEM_CODE_ADD, new MenuItemWarehouseAddIngredient(WAREHOUSE_MENU_ITEM_TEXT_ADD));
-            put(WAREHOUSE_MENU_ITEM_CODE_DELETE, new MenuItemWarehouseTakeIngredient(WAREHOUSE_MENU_ITEM_TEXT_DELETE));
-            put(WAREHOUSE_MENU_ITEM_CODE_FIND, new MenuItemWarehouseFind(WAREHOUSE_MENU_ITEM_TEXT_FIND));
-            put(WAREHOUSE_MENU_ITEM_CODE_LIST, new MenuItemWarehouseList(WAREHOUSE_MENU_ITEM_TEXT_LIST));
-            put(WAREHOUSE_MENU_ITEM_CODE_ELAPSING_LIST, new MenuItemWarehouseElapsingList(WAREHOUSE_MENU_ITEM_TEXT_ELAPSING_LIST));
-            put(WAREHOUSE_MENU_ITEM_CODE_EXIT, new MenuItemEmptyAction(MENU_ITEM_TEXT_EXIT));
-        }
-    };
-    */
 
     private static RestaurantController staticRestaurantController;
     private MenuItem mainMenu;
