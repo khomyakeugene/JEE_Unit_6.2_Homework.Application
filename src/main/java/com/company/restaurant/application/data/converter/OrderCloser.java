144f52a0ea4bb7988378ba4774e3cb2efd6f1822
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-02 23:12:04 +0300

    JEE Unit 6.2 Homework Application

--- src/main/java/com/company/restaurant/application/data/converter/OrderCloser.java
+++ src/main/java/com/company/restaurant/application/data/converter/OrderCloser.java
@@ -1,13 +1,12 @@
 package com.company.restaurant.application.data.converter;
 
-import com.company.restaurant.application.data.service.Executor;
 import com.company.restaurant.application.data.service.ObjectProcessorProto;
 import com.company.restaurant.model.Order;
 
 /**
  * Created by Yevhen on 01.06.2016.
  */
-public class OrderCloser extends ObjectProcessorProto<Order> implements Executor {
+public class OrderCloser extends ObjectProcessorProto<Order> {
     private static final String ORDER_HAS_BEEN_CLOSED_PATTERN = "Order with number <%s> has been closed";
 
     @Override
