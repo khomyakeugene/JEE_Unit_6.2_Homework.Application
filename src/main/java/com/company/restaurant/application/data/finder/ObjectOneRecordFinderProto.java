5bbf5737aac165836a282cfc6ce32099546380b9
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-03 10:12:00 +0300

    JEE Unit 6.2 Homework Application

--- src/main/java/com/company/restaurant/application/data/finder/ObjectOneRecordFinderProto.java
+++ src/main/java/com/company/restaurant/application/data/finder/ObjectOneRecordFinderProto.java
@@ -1,8 +1,5 @@
 package com.company.restaurant.application.data.finder;
 
-import com.company.restaurant.application.data.service.ObjectFinderAndChooserProto;
-import com.company.restaurant.application.data.service.Executor;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,38 +7,19 @@ import java.util.List;
  * Created by Yevhen on 28.05.2016.
  */
 public abstract class ObjectOneRecordFinderProto<ObjectType, ObjectKeyFieldType>
-        extends ObjectFinderAndChooserProto<ObjectType, ObjectKeyFieldType>
-        implements Executor {
+        extends ObjectFinderProto<ObjectType, ObjectKeyFieldType> {
 
     protected abstract ObjectType findObject(ObjectKeyFieldType objectKeyFieldValue);
 
-    private List<ObjectType>  displayObjectList(ObjectType object) {
-        List<ObjectType> objects = new ArrayList<>();
+    @Override
+    protected List<ObjectType> findObjects(ObjectKeyFieldType objectKeyFieldValue) {
+        List<ObjectType> result = new ArrayList<>();
+
+        ObjectType object = findObject(objectKeyFieldValue);
         if (object != null) {
-            objects.add(object);
-        }
-
-        return objectTableList.displayObjectList(objects);
-    }
-
-    private List<ObjectType> findAndDisplayObjectList() {
-        List<ObjectType> result = null;
-
-        ObjectKeyFieldType objectKeyFieldValue = readKeyFieldValue();
-        if (objectKeyFieldValue != null) {
-            ObjectType object = findObject(objectKeyFieldValue);
-            if (object != null) {
-                result = displayObjectList(object);
-            } else {
-                objectDataHasNotBeenFoundMessage();
-            }
+            result.add(object);
         }
 
         return result;
-    }
-
-    @Override
-    public void execute() {
-        findAndDisplayObjectList();
     }
 }
