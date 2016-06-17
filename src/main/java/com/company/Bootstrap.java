381af93b7d32d9c4bbe31515c60b2fc0b683ac71
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-17 21:49:47 +0300

    Restaurant Application

--- src/main/java/com/company/Bootstrap.java
+++ src/main/java/com/company/Bootstrap.java
@@ -6,11 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Bootstrap {
     private final static String APPLICATION_CONTEXT_NAME = "application-context.xml";
-    private final static String RESTAURANT_APPLICATION_BEAN_NAME = "restaurantConsoleApplication";
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_NAME);
-        RestaurantConsoleApplication restaurantConsoleApplication = context.getBean(RESTAURANT_APPLICATION_BEAN_NAME,
+        RestaurantConsoleApplication restaurantConsoleApplication = context.getBean(
                 RestaurantConsoleApplication.class);
 
         restaurantConsoleApplication.run();
