9f7a5e7c2ff7f134e54e5d4c76ece657d07bede2
khomyakeugene <khomyak_eugene@yahoo.com>
2016-07-04 16:32:30 +0300

    Restaurant Application

--- src/main/java/com/company/restaurant/application/data/viewer/EmployeeOrdersAndCookedCoursesViewer.java
+++ src/main/java/com/company/restaurant/application/data/viewer/EmployeeOrdersAndCookedCoursesViewer.java
@@ -1,20 +1,29 @@
 package com.company.restaurant.application.data.viewer;
 
 import com.company.restaurant.application.data.list.EmployeeCookedCourseTableList;
+import com.company.restaurant.application.data.list.EmployeeOrderTableList;
 import com.company.restaurant.application.data.service.Executor;
 import com.company.restaurant.application.data.service.ObjectProcessorProto;
 import com.company.restaurant.model.Employee;
+import com.company.util.Util;
 
 /**
  * Created by Yevhen on 04.07.2016.
  */
 public class EmployeeOrdersAndCookedCoursesViewer
         extends ObjectProcessorProto<Employee> implements Executor {
+    private static final String EMPLOYEE_COOKED_COURSES_LIST_PATTERN = "Courses, cooked by <%s>:";
+    private static final String EMPLOYEE_ORDERS_LIST_PATTERN = "Orders, processed by <%s>:";
 
     private EmployeeCookedCourseTableList employeeCookedCourseTableList;
+    private EmployeeOrderTableList employeeOrderTableList;
 
     public void setEmployeeCookedCourseTableList(EmployeeCookedCourseTableList employeeCookedCourseTableList) {
         this.employeeCookedCourseTableList = employeeCookedCourseTableList;
+    }
+
+    public void setEmployeeOrderTableList(EmployeeOrderTableList employeeOrderTableList) {
+        this.employeeOrderTableList = employeeOrderTableList;
     }
 
     @Override
@@ -26,9 +35,12 @@ public class EmployeeOrdersAndCookedCoursesViewer
     @Override
     protected String processObject(Employee employee) {
         // Show employee orders
+        Util.printMessage(String.format(EMPLOYEE_ORDERS_LIST_PATTERN, employee.getName()));
+        employeeOrderTableList.displayItemList(employee);
 
         // Show employee cooked courses
-        employeeCookedCourseTableList.displayObjectList();
+        Util.printMessage(String.format(EMPLOYEE_COOKED_COURSES_LIST_PATTERN, employee.getName()));
+        employeeCookedCourseTableList.displayItemList(employee);
 
         return null;
     }
